e979e0d879d0469a1150b158fdb93598d0563c43
Ponmanoj P <munch@Ponmanojs-MacBook-Air.local>
2025-09-21 21:36:41 +0530

    once more

--- PW1/Data_Validator_Frontend/vite.config.js
+++ PW1/Data_Validator_Frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // allow access from network
+  },
+  preview: {
+    host: true,
+  },
+  // Allow your Render domain
+  // This is for production build, so requests from this host are accepted
+  server: {
+    allowedHosts: ["pwfrontend.onrender.com", "localhost", "127.0.0.1"]
+  }
+});
